c8e52cbbd3d98b6da066f7007508a0f632db277f
kaderno <kaderno@gmail.com>
2020-03-09 18:54:11 +1100

    Added helm3 option
    updated password generation
    
    Signed-off-by: kadern0 <kaderno@gmail.com>

--- cmd/apps/minio_app.go
+++ cmd/apps/minio_app.go
@@ -33,12 +33,14 @@ func MakeInstallMinio() *cobra.Command {
 	minio.Flags().String("secret-key", "", "Provide a secret key to override the pre-generated value")
 	minio.Flags().Bool("distributed", false, "Deploy Minio in Distributed Mode")
 	minio.Flags().String("namespace", "default", "Kubernetes namespace for the application")
+	minio.Flags().Bool("helm3", true, "Use helm3, if set to false uses helm2")
 	minio.Flags().Bool("persistence", false, "Enable persistence")
 	minio.Flags().StringArray("set", []string{},
 		"Use custom flags or override existing flags \n(example --set persistence.enabled=true)")
 
 	minio.RunE = func(command *cobra.Command, args []string) error {
 		kubeConfigPath := getDefaultKubeconfig()
+		wait, _ := command.Flags().GetBool("wait")
 
 		if command.Flags().Changed("kubeconfig") {
 			kubeConfigPath, _ = command.Flags().GetString("kubeconfig")
@@ -46,6 +48,11 @@ func MakeInstallMinio() *cobra.Command {
 		updateRepo, _ := minio.Flags().GetBool("update-repo")
 
 		fmt.Printf("Using kubeconfig: %s\n", kubeConfigPath)
+		helm3, _ := command.Flags().GetBool("helm3")
+
+		if helm3 {
+			fmt.Println("Using helm3")
+		}
 
 		arch := getNodeArchitecture()
 		fmt.Printf("Node architecture: %q\n", arch)
@@ -72,20 +79,25 @@ func MakeInstallMinio() *cobra.Command {
 			return fmt.Errorf("please use the helm chart if you'd like to change the namespace to %s", ns)
 		}
 
-		_, err = helm.TryDownloadHelm(userPath, clientArch, clientOS, false)
+		_, err = helm.TryDownloadHelm(userPath, clientArch, clientOS, helm3)
+		if err != nil {
+			return err
+		}
+
+		err = addHelmRepo("stable", "https://kubernetes-charts.storage.googleapis.com", helm3)
 		if err != nil {
 			return err
 		}
 
 		if updateRepo {
-			err = updateHelmRepos(false)
+			err = updateHelmRepos(helm3)
 			if err != nil {
 				return err
 			}
 		}
 
 		chartPath := path.Join(os.TempDir(), "charts")
-		err = fetchChart(chartPath, "stable/minio", defaultVersion, false)
+		err = fetchChart(chartPath, "stable/minio", defaultVersion, helm3)
 
 		if err != nil {
 			return err
@@ -97,13 +109,20 @@ func MakeInstallMinio() *cobra.Command {
 		accessKey, _ := minio.Flags().GetString("access-key")
 		secretKey, _ := minio.Flags().GetString("secret-key")
 
+		gen, err := password.NewGenerator(&password.GeneratorInput{
+			Symbols: "+/",
+		})
+		if err != nil {
+			return err
+		}
+
 		if len(accessKey) == 0 {
 			fmt.Printf("Access Key not provided, one will be generated for you\n")
-			accessKey, err = password.Generate(20, 10, 0, false, true)
+			accessKey, err = gen.Generate(20, 10, 0, false, true)
 		}
 		if len(secretKey) == 0 {
 			fmt.Printf("Secret Key not provided, one will be generated for you\n")
-			secretKey, err = password.Generate(40, 10, 5, false, true)
+			secretKey, err = gen.Generate(40, 10, 5, false, true)
 		}
 
 		if err != nil {
@@ -128,23 +147,38 @@ func MakeInstallMinio() *cobra.Command {
 			return err
 		}
 
-		outputPath := path.Join(chartPath, "minio/rendered")
+		if helm3 {
+			outputPath := path.Join(chartPath, "minio")
 
-		err = templateChart(chartPath,
-			"minio",
-			ns,
-			outputPath,
-			"values.yaml",
-			overrides)
+			err := helm3Upgrade(outputPath, "stable/minio", ns,
+				"values.yaml",
+				defaultVersion,
+				overrides,
+				wait)
 
-		if err != nil {
-			return err
-		}
+			if err != nil {
+				return err
+			}
 
-		err = kubectl("apply", "-R", "-f", outputPath)
+		} else {
+			outputPath := path.Join(chartPath, "minio/rendered")
 
-		if err != nil {
-			return err
+			err = templateChart(chartPath,
+				"minio",
+				ns,
+				outputPath,
+				"values.yaml",
+				overrides)
+
+			if err != nil {
+				return err
+			}
+
+			err = kubectl("apply", "-R", "-f", outputPath)
+
+			if err != nil {
+				return err
+			}
 		}
 
 		fmt.Println(minioInstallMsg)
