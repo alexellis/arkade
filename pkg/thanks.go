1d497d6ec3a84422f7446ee4666463dc90c05b7c
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-03-09 12:33:42 +0000

    Add license to pkg files
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- pkg/thanks.go
+++ pkg/thanks.go
@@ -1,3 +1,7 @@
+// Copyright (c) arkade author(s) 2020. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package pkg
 
+// ThanksForUsing message is printed after installing apps
 const ThanksForUsing = `Thanks for using arkade!`
