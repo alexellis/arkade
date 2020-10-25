b7d276a11dc6c7976f48853a7f70d036c7989f7d
Nitishkumar Singh <nitishkumarsingh71@gmail.com>
2020-10-25 08:35:30 +0530

    add app for kong-ingress
    Signed-off-by: Nitishkumar Singh <nitishkumarsingh71@gmail.com>

--- cmd/install.go
+++ cmd/install.go
@@ -103,7 +103,7 @@ func GetApps() map[string]ArkadeApp {
 	arkadeApps["consul-connect"] = NewArkadeApp(apps.MakeInstallConsul, apps.ConsulInfoMsg)
 	arkadeApps["sealed-secret"] = NewArkadeApp(apps.MakeInstallSealedSecrets, apps.SealedSecretsInfoMsg)
 	arkadeApps["gitlab"] = NewArkadeApp(apps.MakeInstallGitLab, apps.GitlabInfoMsg)
-
+	arkadeApps["kong-ingress"] = NewArkadeApp(apps.MakeInstallKongIngress, apps.KongIngressInfoMsg)
 	// Special "chart" app - let a user deploy any helm chart
 	arkadeApps["chart"] = NewArkadeApp(apps.MakeInstallChart, "")
 	return arkadeApps
