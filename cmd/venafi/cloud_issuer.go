64069deb2d4c7b6c07aac12ec5e233071dcb7dba
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-11-08 18:23:48 +0000

    Update the cloud-issuer to create resources
    This commit was tested against Venafi Cloud and it created the
    issuer as expected and said that it "had started"
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- cmd/venafi/cloud_issuer.go
+++ cmd/venafi/cloud_issuer.go
@@ -1,8 +1,15 @@
 package venafi
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+	"text/template"
 
+	"github.com/alexellis/arkade/pkg/k8s"
 	"github.com/spf13/cobra"
 )
 
@@ -13,14 +20,17 @@ func MakeCloudIssuer() *cobra.Command {
 		Use:   "cloud-issuer",
 		Short: "Install the cert-manager issuer for Venafi cloud",
 		Long: `Install the cert-manager issuer for Venafi cloud to obtain 
-TLS certificates from enterprise-grade CAs.`,
+TLS certificates from enterprise-grade CAs.
+
+Register and download your secret from your dashboard at:
+https://www.venafi.com/venaficloud/devopsaccelerate`,
 		Example: `  arkade venafi install cloud-issuer
   arkade venafi install cloud-issuer --help`,
 		SilenceUsage: true,
 	}
 
-	command.Flags().String("cloud-secret", "", "Your Venafi cloud secret")
-	command.Flags().StringP("cloud-secret-file", "f", "", "Your Venafi cloud secret from a file")
+	command.Flags().String("secret", "", "Your Venafi cloud secret")
+	command.Flags().StringP("secret-file", "f", "", "Your Venafi cloud secret from a file")
 	command.Flags().String("namespace", "default", "Namespace for the issuer")
 	command.Flags().String("name", "cloud-venafi-issuer", "Name for the issuer")
 	command.Flags().StringP("zone", "z", "", "The zone for Venafi cloud")
@@ -44,16 +54,88 @@ TLS certificates from enterprise-grade CAs.`,
 			return fmt.Errorf("a zone is required")
 		}
 
+		// 	kubectl create secret generic \
+		//    cloud-secret \
+		//    --namespace='NAMESPACE OF YOUR ISSUER RESOURCE' \
+		//    --from-literal=apikey='YOUR_CLOUD_API_KEY_HERE'
+
+		tokenFileName, _ := command.Flags().GetString("secret-file")
+		tokenString, _ := command.Flags().GetString("secret")
+
+		var accessKeyFrom, accessKeyValue string
+		if len(tokenFileName) > 0 {
+			accessKeyFrom = "--from-file"
+			accessKeyValue = tokenFileName
+		} else if len(tokenString) > 0 {
+			accessKeyFrom = "--from-literal"
+			accessKeyValue = tokenString
+		} else {
+			return fmt.Errorf(`--secret or secret-file is a required`)
+		}
+
 		fmt.Printf(`Installing the cloud-issuer for you now.
 Name: %s
 Namespace: %s
 Zone: %s
 
 `, name, namespace, zone)
-		// 	kubectl create secret generic \
-		//    cloud-secret \
-		//    --namespace='NAMESPACE OF YOUR ISSUER RESOURCE' \
-		//    --from-literal=apikey='YOUR_CLOUD_API_KEY_HERE'
+		clusterSecretName := name + "-secret"
+		res, err := k8s.KubectlTask("create", "secret", "generic",
+			clusterSecretName,
+			"--namespace="+namespace,
+			accessKeyFrom, "apikey="+accessKeyValue)
+
+		if err != nil {
+			return err
+		} else if len(res.Stderr) > 0 && strings.Contains(res.Stderr, "AlreadyExists") {
+			fmt.Printf("[Warning] secret %s already exists and will be used\n", clusterSecretName)
+		} else if len(res.Stderr) > 0 {
+			return fmt.Errorf("error from kubectl\n%q", res.Stderr)
+		}
+
+		tmpl, err := template.New("yaml").Parse(issuerTemplate)
+
+		if err != nil {
+			return err
+		}
+
+		var tpl bytes.Buffer
+
+		err = tmpl.Execute(&tpl, struct {
+			Name      string
+			Namespace string
+			Zone      string
+		}{
+			Name:      name,
+			Namespace: namespace,
+			Zone:      zone,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		d := os.TempDir()
+		p := path.Join(d, "issuer.yaml")
+
+		err = ioutil.WriteFile(p, tpl.Bytes(), os.ModePerm)
+		if err != nil {
+			return err
+		}
+
+		res, err = k8s.KubectlTask("apply", "-f", p)
+
+		if err != nil {
+			return err
+		}
+
+		if res.ExitCode != 0 {
+			return fmt.Errorf(`unable to apply %s, error: %s`, p, res.Stderr)
+		}
+		fmt.Println(res.Stdout)
+
+		// Check for error, mention to install cert-manager
+		//no matches for kind "Issuer"
 
 		fmt.Println(`# Query the status of the issuer:
 kubectl get issuer ` + name + ` -n ` + namespace + ` -o wide
@@ -81,9 +163,9 @@ metadata:
   namespace: {{.Namespace}}
 spec:
   venafi:
-    zone: "{{.Zone}}" # Set this to the Venafi policy zone you want to use
+    zone: "{{.Zone}}"
     cloud:
       apiTokenSecretRef:
-        name: cloud-secret
-		key: apikey
+        name: {{.Name}}-secret
+        key: apikey
 `
