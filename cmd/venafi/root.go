d4c4d22c592182fbf64c3e0f679cd30307774210
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-11-09 12:30:24 +0000

    Update headers in each source file
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- cmd/venafi/root.go
+++ cmd/venafi/root.go
@@ -1,6 +1,7 @@
 // Copyright (c) arkade author(s) 2020. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+// venafi contains a suite of Sponsored Apps for arkade
 package venafi
 
 import (
