d1d02d132933f51cf424e344ab0650e9e5fe4ffd
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2021-03-04 11:22:40 +0000

    Add mqtt-connector to arkade for openfaas
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ cmd/apps/mqttconnector_app.go
@@ -0,0 +1,112 @@
+// Copyright (c) arkade author(s) 2020. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package apps
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/alexellis/arkade/pkg/apps"
+	"github.com/alexellis/arkade/pkg/types"
+
+	"github.com/alexellis/arkade/pkg"
+	"github.com/spf13/cobra"
+)
+
+func MakeInstallMQTTConnector() *cobra.Command {
+	var command = &cobra.Command{
+		Use:          "mqtt-connector",
+		Short:        "Install mqtt-connector for OpenFaaS",
+		Long:         `Install mqtt-connector for OpenFaaS`,
+		Example:      `  arkade install mqtt-connector`,
+		SilenceUsage: true,
+	}
+
+	command.Flags().StringP("namespace", "n", "openfaas", "The namespace used for installation")
+	command.Flags().Bool("update-repo", true, "Update the helm repo")
+	command.Flags().StringP("topics", "t", "", "The topics for the connector to bind to, currently supports one topic")
+	command.Flags().String("broker-host", "tcp://test.mosquitto.org:1883", "The host for the MQTT broker")
+	command.Flags().String("client-id", "mqtt-connector-1", "The client ID for the MQTT broker")
+	command.Flags().Bool("async", false, "Invoke functions asynchronously from events")
+	command.Flags().StringArray("set", []string{},
+		"Use custom flags or override existing flags \n(example --set key=value)")
+
+	command.RunE = func(command *cobra.Command, args []string) error {
+		kubeConfigPath, _ := command.Flags().GetString("kubeconfig")
+
+		updateRepo, _ := command.Flags().GetBool("update-repo")
+		async, err := command.Flags().GetBool("async")
+		if err != nil {
+			return err
+		}
+
+		namespace, _ := command.Flags().GetString("namespace")
+		clientID, _ := command.Flags().GetString("client-id")
+		topicsVal, err := command.Flags().GetString("topics")
+		if err != nil {
+			return err
+		}
+
+		brokerHostVal, err := command.Flags().GetString("broker-host")
+		if err != nil {
+			return err
+		}
+
+		overrides := map[string]string{
+			"topic":       topicsVal,
+			"broker":      brokerHostVal,
+			"clientID":    clientID,
+			"asyncInvoke": strconv.FormatBool(async),
+		}
+
+		customFlags, err := command.Flags().GetStringArray("set")
+		if err != nil {
+			return fmt.Errorf("error with --set usage: %s", err)
+		}
+
+		if err := mergeFlags(overrides, customFlags); err != nil {
+			return err
+		}
+
+		if len(topicsVal) == 0 {
+			return fmt.Errorf("--topics is required")
+		}
+
+		if len(brokerHostVal) == 0 {
+			return fmt.Errorf("--broker-host is required")
+		}
+
+		mqttConnectorAppOptions := types.DefaultInstallOptions().
+			WithNamespace(namespace).
+			WithHelmRepo("openfaas/mqtt-connector").
+			WithHelmURL("https://openfaas.github.io/faas-netes/").
+			WithOverrides(overrides).
+			WithHelmUpdateRepo(updateRepo).
+			WithKubeconfigPath(kubeConfigPath)
+
+		_, err = apps.MakeInstallChart(mqttConnectorAppOptions)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(mqttConnectorInstallMsg)
+
+		return nil
+	}
+
+	return command
+}
+
+const MQTTConnectorInfoMsg = `# View the connector's logs:
+
+kubectl logs deploy/mqtt-connector -n openfaas -f
+
+# Find out more on the project homepage:
+
+# https://github.com/openfaas/mqtt-connector/`
+
+const mqttConnectorInstallMsg = `=======================================================================
+= mqtt-connector has been installed.                                   =
+=======================================================================` +
+	"\n\n" + MQTTConnectorInfoMsg + "\n\n" + pkg.ThanksForUsing
