817999879a7301b14c117d53e4251967ae1f380a
Alistair Hey <alistair@heyal.co.uk>
2021-04-29 18:23:14 +0100

    Bump the nats cli version to 0.23
    Tested locally with go build, and tests run
    
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- pkg/get/tools.go
+++ pkg/get/tools.go
@@ -889,7 +889,7 @@ https://github.com/{{.Owner}}/{{.Repo}}/releases/download/{{.Version}}/{{.Name}}
 			Owner:   "nats-io",
 			Repo:    "natscli",
 			Name:    "nats",
-			Version: "0.0.21",
+			Version: "0.0.23",
 			URLTemplate: `{{$arch := .Arch}}
 			{{ if eq .Arch "x86_64" -}}
 			{{$arch = "amd64"}}
