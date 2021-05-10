05930f5d70818091bfd4575603b5e8d37ae4bdbb
Alistair Hey <alistair@heyal.co.uk>
2021-05-10 09:03:40 +0100

    Update influxdb install message
    Change influxdb install message
    
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- cmd/apps/influxdb_app.go
+++ cmd/apps/influxdb_app.go
@@ -69,17 +69,13 @@ InfluxDB can be accessed via port 8086 on the following DNS name from within you
 
   http://influxdb.default:8086
 
-You can connect to the remote instance with the influx CLI. To forward the API port to localhost:8086, run the following:
+To port forward the API port to localhost:8086, run the following:
 
-  kubectl port-forward --namespace default $(kubectl get pods --namespace default -l app=influxdb -o jsonpath='{ .items[0].metadata.name }') 8086:8086
-
-You can also connect to the influx CLI from inside the container. To open a shell session in the InfluxDB pod, run the following:
-
-  kubectl exec -i -t --namespace default $(kubectl get pods --namespace default -l app=influxdb -o jsonpath='{.items[0].metadata.name}') /bin/sh
+  kubectl port-forward -n default statefulset/influxdb 8086:8086
 
 To view the logs for the InfluxDB pod, run the following:
 
-  kubectl logs -f --namespace default $(kubectl get pods --namespace default -l app=influxdb -o jsonpath='{ .items[0].metadata.name }')
+  kubectl logs -f -n default statefulset/influxdb
 
 `
 
