2b021330bb11559fdd2de7a70c080052e76a791c
Carlos Panato <ctadeu@gmail.com>
2021-05-28 18:21:25 +0200

    update tests based on review feeback
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- cmd/apps/inletsoperator_app_test.go
+++ cmd/apps/inletsoperator_app_test.go
@@ -6,97 +6,101 @@ package apps
 import (
 	"io/ioutil"
 	"os"
-	"reflect"
+	"strings"
 	"testing"
 )
 
-func Test_ProviderHetzner(t *testing.T) {
-	cmdInlets := MakeInstallInletsOperator()
-
-	// missing required flags
-	err := cmdInlets.ParseFlags([]string{"--provider", "hetzner"})
-	if err != nil {
-		t.Errorf("expected no error when parsing flags, but got: %s", err.Error())
+func Test_ValidatePreRun(t *testing.T) {
+	testcases := []struct {
+		name              string
+		expectedError     string
+		token             string
+		tokenFileName     string
+		secretKeyFileName string
+	}{
+		{
+			name:              "token is set",
+			expectedError:     "",
+			token:             "my_honk_token",
+			tokenFileName:     "",
+			secretKeyFileName: "",
+		},
+		{
+			name:              "tokenFileName is set",
+			expectedError:     "",
+			token:             "",
+			tokenFileName:     "honk-token-file",
+			secretKeyFileName: "",
+		},
+		{
+			name:              "tokenFileName and secretKeyFileName are set",
+			expectedError:     "",
+			token:             "",
+			tokenFileName:     "honk-token-file",
+			secretKeyFileName: "honk-secret-file",
+		},
+		{
+			name:              "missing token",
+			expectedError:     "--token-file or --token is a required field for your cloud API token or service account JSON",
+			token:             "",
+			tokenFileName:     "",
+			secretKeyFileName: "",
+		},
+		{
+			name:              "invalid token file",
+			expectedError:     "failed to check the token file invalid-token-file: stat invalid-token-file: no such file or directory",
+			token:             "",
+			tokenFileName:     "invalid-token-file",
+			secretKeyFileName: "",
+		},
+		{
+			name:              "invalid secret file",
+			expectedError:     "failed to check the secret file invalid-secret-file: stat invalid-secret-file: no such file or directory",
+			token:             "honk-token",
+			tokenFileName:     "",
+			secretKeyFileName: "invalid-secret-file",
+		},
 	}
 
-	errWant := "invalid region set for provider hetzner. Valid regions are: fsn1, nbg1, hel1"
-	_, err = getInletsOperatorOverrides(cmdInlets)
-	if err == nil {
-		t.Errorf("expected error when missing a flag")
-	}
-	if err.Error() != errWant {
-		t.Errorf("expected error: %s, got: %s", errWant, err)
-	}
+	for _, tc := range testcases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
 
-	// required flags in place
-	err = cmdInlets.ParseFlags([]string{"--provider", "hetzner", "--region", "fsn1"})
-	if err != nil {
-		t.Errorf("expected no error when parsing flags, but got: %s", err.Error())
-	}
+			tokenTempfileName := ""
+			if strings.Contains(tc.tokenFileName, "invalid") {
+				tokenTempfileName = tc.tokenFileName
+			} else if tc.tokenFileName != "" {
+				tokenTempfileName = createTempFile(t, tc.tokenFileName)
+				defer os.Remove(tokenTempfileName)
+			}
 
-	overridesWant := map[string]string{"provider": "hetzner", "region": "fsn1"}
-	overrides, err := getInletsOperatorOverrides(cmdInlets)
-	if err != nil {
-		t.Errorf("expected no error when parsing flags, but got: %s", err.Error())
-	}
-	if !reflect.DeepEqual(overrides, overridesWant) {
-		t.Errorf("expected %v, but got: %v", overridesWant, overrides)
+			secretTempfileName := ""
+			if strings.Contains(tc.secretKeyFileName, "invalid") {
+				secretTempfileName = tc.secretKeyFileName
+			} else if tc.secretKeyFileName != "" {
+				secretTempfileName = createTempFile(t, tc.secretKeyFileName)
+				defer os.Remove(secretTempfileName)
+			}
+
+			err := validatePreRun(tc.token, tokenTempfileName, secretTempfileName)
+			if tc.expectedError == "" {
+				if err != nil {
+					t.Errorf("expected no error when validating the PreRun, but got: %s", err.Error())
+				}
+			} else {
+				if err.Error() != tc.expectedError {
+					t.Errorf("expected error: %s, got: %s", tc.expectedError, err)
+				}
+			}
+		})
 	}
 }
 
-func Test_ValidatePreRun(t *testing.T) {
-	err := validatePreRun("my_honk_token", "", "")
-	if err != nil {
-		t.Errorf("expected no error when validating the PreRun, but got: %s", err.Error())
-	}
-
-	tokenFile, err := ioutil.TempFile("", "honk-token-file")
+func createTempFile(t *testing.T, name string) string {
+	tempFile, err := ioutil.TempFile("", name)
 	if err != nil {
 		t.Errorf("expected no error when creating the temporary test file, but got: %s", err.Error())
 	}
-	defer os.Remove(tokenFile.Name())
 
-	err = validatePreRun("", tokenFile.Name(), "")
-	if err != nil {
-		t.Errorf("expected no error when validating the PreRun, but got: %s", err.Error())
-	}
-
-	secretFile, err := ioutil.TempFile("", "honk-secret-file")
-	if err != nil {
-		t.Errorf("expected no error when creating the temporary test file, but got: %s", err.Error())
-	}
-	defer os.Remove(secretFile.Name())
-
-	err = validatePreRun("", tokenFile.Name(), secretFile.Name())
-	if err != nil {
-		t.Errorf("expected no error when validating the PreRun, but got: %s", err.Error())
-	}
-
-	// Invalid TokenFileName
-	errWant := "failed to check the token file invalid-token-file: stat invalid-token-file: no such file or directory"
-	err = validatePreRun("", "invalid-token-file", "")
-	if err == nil {
-		t.Errorf("expected error when the token file does not exist")
-	}
-	if err.Error() != errWant {
-		t.Errorf("expected error: %s, got: %s", errWant, err)
-	}
-
-	errWant = "failed to check the secret file invalid-secret-file: stat invalid-secret-file: no such file or directory"
-	err = validatePreRun("my_honk_token", "", "invalid-secret-file")
-	if err == nil {
-		t.Errorf("expected error when the secret file does not exist")
-	}
-	if err.Error() != errWant {
-		t.Errorf("expected error: %s, got: %s", errWant, err)
-	}
-
-	errWant = "--token-file or --token is a required field for your cloud API token or service account JSON"
-	err = validatePreRun("", "", "")
-	if err == nil {
-		t.Errorf("expected error when the secret file does not exist")
-	}
-	if err.Error() != errWant {
-		t.Errorf("expected error: %s, got: %s", errWant, err)
-	}
+	return tempFile.Name()
 }
