934e95489f7aaf86056b770b72ba70a3213655cd
Czékus Máté <zcmate@gmail.com>
2022-01-20 19:58:58 +0100

    Add `kumactl` to installable tools with e2e skip
    Signed-off-by: Czékus Máté <zcmate@gmail.com>
    Signed-off-by: Czékus Máté <mate@picloud.hu>

--- pkg/get/get_test.go
+++ pkg/get/get_test.go
@@ -3418,3 +3418,37 @@ func Test_DownloadDagger(t *testing.T) {
 		}
 	}
 }
+
+func Test_DownloadKumactl(t *testing.T) {
+	tools := MakeTools()
+	name := "kumactl"
+
+	tool := getTool(name, tools)
+
+	tests := []test{
+		{
+			os:      "darwin",
+			arch:    arch64bit,
+			version: "1.4.1",
+			url:     "https://download.konghq.com/mesh-alpine/kuma-1.4.1-darwin-amd64.tar.gz",
+		},
+		{
+			os:      "linux",
+			arch:    arch64bit,
+			version: "1.4.1",
+			url:     "https://download.konghq.com/mesh-alpine/kuma-1.4.1-ubuntu-amd64.tar.gz",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.os+" "+tc.arch+" "+tc.version, func(r *testing.T) {
+			got, err := tool.GetURL(tc.os, tc.arch, tc.version, false)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got != tc.url {
+				t.Errorf("want: %s, got: %s", tc.url, got)
+			}
+		})
+	}
+}
