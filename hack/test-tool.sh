bba680780e3e637f55df3701b10e367d1eee3198
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2023-02-25 09:01:47 +0000

    Update test-tool.sh readability
    * Adds spacing between entries for better readability
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- hack/test-tool.sh
+++ hack/test-tool.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-set -e -x -u -o pipefail
+if [ -z $1 ];
+then
+  echo usage: test-tool.sh TOOLNAME
+  exit 1
+fi
 
-go build 
+set -x 
 
-./arkade get $1 --arch arm64 --os darwin --quiet
-file $HOME/.arkade/bin/$1
-rm $HOME/.arkade/bin/$1
+./arkade get $1 --arch arm64 --os darwin --quiet && file $HOME/.arkade/bin/$1 && rm $HOME/.arkade/bin/$1 && echo 
 
-./arkade get $1 --arch x86_64 --os darwin --quiet
-file $HOME/.arkade/bin/$1
-rm $HOME/.arkade/bin/$1 
+./arkade get $1 --arch x86_64 --os darwin --quiet && file $HOME/.arkade/bin/$1 && rm $HOME/.arkade/bin/$1 && echo 
 
-./arkade get $1 --arch x86_64 --os linux --quiet
-file $HOME/.arkade/bin/$1
-rm $HOME/.arkade/bin/$1 
+./arkade get $1 --arch x86_64 --os linux --quiet && file $HOME/.arkade/bin/$1 && rm $HOME/.arkade/bin/$1 && echo 
 
-./arkade get $1 --arch arm64 --os linux --quiet
-file $HOME/.arkade/bin/$1
-rm $HOME/.arkade/bin/$1 
+./arkade get $1 --arch arm64 --os linux --quiet && file $HOME/.arkade/bin/$1 && rm $HOME/.arkade/bin/$1 && echo 
 
-./arkade get $1 --arch x86_64 --os ming --quiet
-file $HOME/.arkade/bin/$1
-rm $HOME/.arkade/bin/$1 
+./arkade get $1 --arch x86_64 --os ming --quiet && file $HOME/.arkade/bin/$1 && rm $HOME/.arkade/bin/$1 && echo 
+
