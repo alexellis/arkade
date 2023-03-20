459cbfc4d4b01049cbbc1053d401d8b063e61dbb
Alex Ellis <alexellis2@gmail.com>
2023-03-20 17:37:13 +0000

    Update test-tool.sh
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- hack/test-tool.sh
+++ hack/test-tool.sh
@@ -14,7 +14,7 @@ set -x
 
 ./arkade get $1 --arch x86_64 --os linux --quiet && file $HOME/.arkade/bin/$1 && rm $HOME/.arkade/bin/$1 && echo 
 
-./arkade get $1 --arch arm64 --os linux --quiet && file $HOME/.arkade/bin/$1 && rm $HOME/.arkade/bin/$1 && echo 
+./arkade get $1 --arch aarch64 --os linux --quiet && file $HOME/.arkade/bin/$1 && rm $HOME/.arkade/bin/$1 && echo 
 
 ./arkade get $1 --arch x86_64 --os ming --quiet && file $HOME/.arkade/bin/$1 && rm $HOME/.arkade/bin/$1 && echo 
 
