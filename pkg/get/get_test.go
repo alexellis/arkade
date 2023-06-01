700ff0d9fb9744a7adff7b425dbfac079a9fce28
Christophe Jauffret <reg-github@geo6.net>
2023-06-01 08:51:23 +0200

    add kube-burner
    Signed-off-by: Christophe Jauffret <reg-github@geo6.net>

--- pkg/get/get_test.go
+++ pkg/get/get_test.go
@@ -6081,3 +6081,54 @@ func Test_DownloadKtop(t *testing.T) {
 		}
 	}
 }
+func Test_DownloadKubeBurner(t *testing.T) {
+	tools := MakeTools()
+	name := "kube-burner"
+
+	tool := getTool(name, tools)
+
+	const toolVersion = "v1.6"
+
+	tests := []test{
+		{
+			os:      "linux",
+			arch:    arch64bit,
+			version: toolVersion,
+			url:     `https://github.com/cloud-bulldozer/kube-burner/releases/download/v1.6/kube-burner-1.6-Linux-x86_64.tar.gz`,
+		},
+		{
+			os:      "darwin",
+			arch:    arch64bit,
+			version: toolVersion,
+			url:     `https://github.com/cloud-bulldozer/kube-burner/releases/download/v1.6/kube-burner-1.6-Darwin-x86_64.tar.gz`,
+		},
+		{
+			os:      "linux",
+			arch:    archARM64,
+			version: toolVersion,
+			url:     `https://github.com/cloud-bulldozer/kube-burner/releases/download/v1.6/kube-burner-1.6-Linux-arm64.tar.gz`,
+		},
+		{
+			os:      "darwin",
+			arch:    archDarwinARM64,
+			version: toolVersion,
+			url:     `https://github.com/cloud-bulldozer/kube-burner/releases/download/v1.6/kube-burner-1.6-Darwin-arm64.tar.gz`,
+		},
+		{
+			os:      "ming",
+			arch:    arch64bit,
+			version: toolVersion,
+			url:     `https://github.com/cloud-bulldozer/kube-burner/releases/download/v1.6/kube-burner-1.6-Windows-x86_64.zip`,
+		},
+	}
+
+	for _, tc := range tests {
+		got, err := tool.GetURL(tc.os, tc.arch, tc.version, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got != tc.url {
+			t.Errorf("want: %s, got: %s", tc.url, got)
+		}
+	}
+}
