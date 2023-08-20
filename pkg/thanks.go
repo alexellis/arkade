69c0dc091a32f2e04d33903d706a7be82fa2fb55
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2023-08-20 10:13:22 +0100

    Add note on actuated
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- pkg/thanks.go
+++ pkg/thanks.go
@@ -4,4 +4,4 @@
 package pkg
 
 // SupportMessageShort shows how to support arkade
-const SupportMessageShort = `🐳 arkade needs your support: https://github.com/sponsors/alexellis`
+const SupportMessageShort = `🐳 Want to speed up your CI 2-3x, whilst lowering costs? https://actuated.dev`
