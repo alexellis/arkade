8b40856efdbb7c0375f09243e63cf5d1dc41fe47
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2023-09-28 16:14:06 +0100

    Enable self-update for arkade
    Tested with Arm64 on Darwin, the newer binary was replaced
    by the one on GitHub.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- cmd/update.go
+++ cmd/update.go
@@ -4,9 +4,17 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/alexellis/arkade/pkg"
+	"github.com/alexellis/arkade/pkg/env"
+	"github.com/alexellis/arkade/pkg/get"
+	"github.com/alexellis/go-execute/v2"
 	"github.com/morikuni/aec"
 	"github.com/spf13/cobra"
 )
@@ -19,25 +27,106 @@ func MakeUpdate() *cobra.Command {
 		Aliases:      []string{"u"},
 		SilenceUsage: false,
 	}
-	command.Run = func(cmd *cobra.Command, args []string) {
-		fmt.Println(arkadeUpdate)
+	command.RunE = func(cmd *cobra.Command, args []string) error {
 
-		fmt.Println("\n", aec.Bold.Apply(pkg.SupportMessageShort))
+		name := "arkade"
+		toolList := get.MakeTools()
+		var tool *get.Tool
+		for _, t := range toolList {
+			if t.Name == name {
+				tool = &t
+				break
+			}
+		}
+
+		release, err := get.FindGitHubRelease("alexellis", name)
+		if err != nil {
+			return err
+		}
+
+		executable, err := os.Executable()
+		if err != nil {
+			return err
+		}
+
+		task := execute.ExecTask{
+			Command: executable,
+			Args:    []string{"version"},
+		}
+
+		res, err := task.Execute(context.TODO())
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Latest release: %s\n", release)
+
+		if strings.Contains(res.Stdout, release) {
+			fmt.Println("You are already using the latest version of arkade.")
+
+			fmt.Println("\n\n", aec.Bold.Apply(pkg.SupportMessageShort))
+
+			return nil
+		}
+
+		arch, operatingSystem := env.GetClientArch()
+		arch = strings.ToLower(arch)
+		operatingSystem = strings.ToLower(operatingSystem)
+
+		if arch == "x86_64" {
+			arch = "amd64"
+		}
+
+		downloadUrl, err := get.GetDownloadURL(tool, operatingSystem, arch, release, false)
+		if err != nil {
+			return err
+		}
+
+		binary, err := get.DownloadFileP(downloadUrl, true)
+		if err != nil {
+			return err
+		}
+
+		if err := replaceExec(executable, binary); err != nil {
+			return err
+		}
+
+		fmt.Printf("Replaced: %s.. OK.", executable)
+
+		fmt.Println("\n\n", aec.Bold.Apply(pkg.SupportMessageShort))
+		return nil
 	}
 	return command
 }
 
-const arkadeUpdate = `You can update arkade with the following:
+// Copy the new binary to the same directory as the current binary before calling os.Rename to prevent an
+// 'invalid cross-device link' error because the source and destination are not on the same file system.
+func replaceExec(currentExec, newBinary string) error {
+	targetDir := filepath.Dir(currentExec)
+	filename := filepath.Base(currentExec)
+	newExec := filepath.Join(targetDir, fmt.Sprintf(".%s.new", filename))
 
-# Remove cached versions of tools
-rm -rf $HOME/.arkade
+	// Copy the contents of newbinary to a new executable file
+	sf, err := os.Open(newBinary)
+	if err != nil {
+		return err
+	}
+	defer sf.Close()
 
-# For Linux/MacOS:
-curl -SLfs https://get.arkade.dev | sudo sh
+	df, err := os.OpenFile(newExec, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0755)
+	if err != nil {
+		return err
+	}
+	defer df.Close()
 
-# For Windows (using Git Bash)
-curl -SLfs https://get.arkade.dev | sh
+	if _, err := io.Copy(df, sf); err != nil {
+		return err
+	}
 
-# Or download from GitHub: https://github.com/alexellis/arkade/releases
+	// Replace the current executable file with the new executable file
+	if err := os.Rename(newExec, currentExec); err != nil {
+		return err
+	}
 
-Thanks for using arkade!`
+	return nil
+}
