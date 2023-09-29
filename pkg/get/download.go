8f028584fc537ec79e0e1f70d7299ca5107765a8
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2023-09-29 16:05:45 +0100

    Remove temporary files after downloading binaries
    A user complained that when using a non ephemeral GitHub
    Actions runner, they needed to delete temporary files from
    /tmp.
    
    Fixes: https://github.com/alexellis/arkade-get/issues/63
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- pkg/get/download.go
+++ pkg/get/download.go
@@ -89,9 +89,15 @@ func Download(tool *Tool, arch, operatingSystem, version string, movePath string
 	if !quiet {
 		log.Printf("Copying %s to %s\n", outFilePath, localPath)
 	}
-	_, err = CopyFile(outFilePath, localPath)
-	if err != nil {
+
+	if _, err = CopyFile(outFilePath, localPath); err != nil {
 		return "", "", err
+	}
+
+	// Remove parent folder of the binary
+	tempPath := filepath.Dir(outFilePath)
+	if err := os.RemoveAll(tempPath); err != nil {
+		log.Printf("Error removing temporary directory: %s", err)
 	}
 
 	outFilePath = localPath
