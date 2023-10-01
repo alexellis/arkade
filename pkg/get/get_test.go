09bb3ae09be36170e2f4a91bab05070f74a83e2c
Tiago Santos <tvcsantos@gmail.com>
2023-10-01 09:38:08 +0100

    feat: add kube-linter tool
    Signed-off-by: Tiago Santos <tvcsantos@gmail.com>

--- pkg/get/get_test.go
+++ pkg/get/get_test.go
@@ -5143,6 +5143,47 @@ func Test_DownloadKubeconform(t *testing.T) {
 	}
 }
 
+func Test_DownloadKubeLinter(t *testing.T) {
+	tools := MakeTools()
+	name := "kube-linter"
+	version := "v0.6.4"
+
+	tool := getTool(name, tools)
+
+	tests := []test{
+		{
+			os:      "darwin",
+			arch:    arch64bit,
+			version: version,
+			url:     "https://github.com/stackrox/kube-linter/releases/download/v0.6.4/kube-linter-darwin",
+		},
+		{
+			os:      "linux",
+			arch:    arch64bit,
+			version: version,
+			url:     "https://github.com/stackrox/kube-linter/releases/download/v0.6.4/kube-linter-linux",
+		},
+		{
+			os:      "ming",
+			arch:    arch64bit,
+			version: version,
+			url:     "https://github.com/stackrox/kube-linter/releases/download/v0.6.4/kube-linter.exe",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.os+" "+tc.arch+" "+tc.version, func(r *testing.T) {
+			got, err := tool.GetURL(tc.os, tc.arch, tc.version, false)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got != tc.url {
+				t.Errorf("want: %s, got: %s", tc.url, got)
+			}
+		})
+	}
+}
+
 func Test_DownloadConftest(t *testing.T) {
 	tools := MakeTools()
 	name := "conftest"
